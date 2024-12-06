4f8981d41164bb37a61dfd9913869815ff5dda07
Иван Хрулев <ivankhrulev@yandex.ru>
2024-12-06 20:01:28 +0300

    Singtest

---
+++ singleton/singleton/Patter_singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    internal class SSSsingleton
+    {
+        private static SSSsingleton instance;
+
+        public static SSSsingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SSSsingleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
